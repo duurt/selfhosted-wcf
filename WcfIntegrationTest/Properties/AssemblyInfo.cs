e9ad0b586f662410970add90fa95853c8a703180
Duurt van der Hoek <dvanderhoek@rdw.nl>
2022-10-05 23:03:20 +0200

    Add project files.

---
+++ WcfIntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WcfIntegrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WcfIntegrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05c28d90-f1e5-4352-ad41-8fec187693d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WcfIntegrationTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ServiceModel;
+
+namespace WcfIntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string uristring = "net.pipe://localhost";
+            var client = new ChannelFactory<ITestService>(
+                    new NetNamedPipeBinding(), 
+                    new EndpointAddress(uristring))
+                .CreateChannel();
+            
+            using (ServiceHost host = new ServiceHost(typeof(TestService), new Uri(uristring)))
+            {
+                host.Open();
+                var result = client.GetMessage();
+                host.Close();
+
+                Assert.AreEqual("Hello", result);
+            }
+        }
+    }
+   
+    [ServiceContract]
+    public interface ITestService
+    {
+        [OperationContract]
+        string GetMessage();
+    }
+    
+    public class TestService : ITestService
+    {
+        public string GetMessage() => "Hello";
+    }
+}
